079ebe3a807afcdfe40509c77c3b6f2ca7b8e052
Damneet Sambhy <damneet.sambhy@hotmail.com>
2023-09-11 23:24:14 +0930

    page started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    title: 'Hukamnama',
+    path: '',
+    loadComponent: () =>
+      import('./components/hukamnama-page/hukamnama-page.component').then(
+        (mod) => mod.HukamnamaPageComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/hukamnama-page/hukamnama-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'hukamnama-page',
+    templateUrl: './hukamnama-page.component.html',
+    styleUrls: ['./hukamnama-page.component.scss']
+})
+export class HukamnamaPageComponent {
+
+}
